e7403ed5c8cf242b14a580bbae43b9b1165f5593
Vincent Cornet <vincent.cornet@gmail.com>
2010-10-19 01:40:34 +0200

    trying to detect areas of same velocity

--- payback/opticalflow/FlowAlgorithm.java
+++ payback/opticalflow/FlowAlgorithm.java
@@ -4,119 +4,175 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class FlowAlgorithm {
-    private BufferedImage[] images = new BufferedImage[2];
-    private float[][] u, v, ex, ey, et, vbar, ubar;
-    private float[][][] e ;
-    private float alpha = .2f;
-    private int h;
-    private int w;
+	/** The previous and current frames. */
+	private BufferedImage[] images = new BufferedImage[2];
+	/** The frame's dimensions. */
+	private int h, w;
 
-    public void update( BufferedImage image ) {
-        images[0] = images[1];
-        h = image.getHeight();
-        w = image.getWidth();
-        BufferedImage newImage = new BufferedImage( w, h, image.getType() );
-        newImage.getGraphics().drawImage( image, 0, 0, null );
-        images[1] = newImage;
+	/** The pixel brightness values, for the previous and current frames. */
+	private float[][][] e;
+	/** Computed values for each pixel. */
+	private float[][] u, v, ex, ey, et, vbar, ubar, norm;
 
-        if(images[0] == null) {
-            return;
-        }
+	/** A magic (constant) value. */
+	private float alpha = .1f;
 
-        if( u == null ) {
-            e = new float[2][w][h];
-            ex = new float[w][h];
-            ey = new float[w][h];
-            et = new float[w][h];
-            ubar = new float[w][h];
-            vbar = new float[w][h];
-            u = new float[w][h];
-            v = new float[w][h];
-        }
+	/** Number of loops in the velocity computation. */
+	private final static int NB_ITERATIONS = 6;
 
-        e[0] = e[1];
-        e[1] = new float[w][h];
-        for( int x = 0; x < w; x++ ) {
-            for( int y = 0; y < h; y++ ) {
-                int rgb = images[1].getRGB( x, y );
-                int r = (rgb & 0xFF0000) >> 16;
-                int g = (rgb & 0xFF00) >> 8;
-                int b = (rgb & 0xFF);
+	/**
+	 * Update all computed values.
+	 * 
+	 * @param image
+	 *            the current frame
+	 */
+	public void update(BufferedImage image) {
+		images[0] = images[1];
+		h = image.getHeight();
+		w = image.getWidth();
+		BufferedImage newImage = new BufferedImage(w, h, image.getType());
+		newImage.getGraphics().drawImage(image, 0, 0, null);
+		images[1] = newImage;
 
-                float[] hsbvals = Color.RGBtoHSB( r, g, b, null );
-                e[1][x][y] = hsbvals[2];
-            }
-        }
+		if (images[0] == null) {
+			return;
+		}
 
-        for( int i = 0; i < w; i++ ) {
-            for( int j = 0; j < h; j++ ) {
-                int x = i; int y =j;
-                if(x == 0)
-                    x = 1;
-                if(x == w-1)
-                    x = w-2;
-                if(y == 0)
-                    y = 1;
-                if(y == h-1)
-                    y = h-2;
-                ex[x][y] = 0.25f * (e[0][x][(y + 1)] - e[0][x][y] + e[0][(x + 1)][(y + 1)] - e[0][(x + 1)][y]
-                                  + e[1][x][(y + 1)] - e[1][x][y] + e[1][(x + 1)][(y + 1)] - e[1][(x + 1)][y]);
-                ey[x][y] = 0.25f * (e[0][(x + 1)][y] - e[0][x][y] + e[0][(x + 1)][(y + 1)] - e[0][x][(y + 1)]
-                                  + e[1][(x + 1)][y] - e[1][x][y] + e[1][(x + 1)][(y + 1)] - e[1][x][(y + 1)]);
-                et[x][y] = 0.25f * (e[1][x][y] - e[0][x][y] + e[1][(x + 1)][y] - e[0][(x + 1)][y]
-                                  + e[1][x][(y + 1)] - e[0][x][(y + 1)] + e[1][(x + 1)][(y + 1)] - e[0][(x + 1)][(y + 1)]);
-            }
-        }
+		if (u == null) {
+			e = new float[2][w][h];
+			ex = new float[w][h];
+			ey = new float[w][h];
+			et = new float[w][h];
+			ubar = new float[w][h];
+			vbar = new float[w][h];
+			u = new float[w][h];
+			v = new float[w][h];
+			norm = new float[w][h];
+		}
 
+		// compute the E values
+		e[0] = e[1];
+		e[1] = new float[w][h];
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				int rgb = images[1].getRGB(x, y);
+				int r = (rgb & 0xFF0000) >> 16;
+				int g = (rgb & 0xFF00) >> 8;
+				int b = (rgb & 0xFF);
 
-        for( int iter = 0; iter < 10; iter++ ) {
+				float[] hsbvals = Color.RGBtoHSB(r, g, b, null);
+				e[1][x][y] = hsbvals[2];
+			}
+		}
 
-            for( int i = 0; i < w; i++ ) {
-                for( int j = 0; j < h; j++ ) {
-                    int x = i; int y =j;
+		// compute the Ex, Ey, Et values
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				int x = i;
+				int y = j;
+				if (x == 0) {
+					x = 1;
+				}
+				if (x == w - 1) {
+					x = w - 2;
+				}
+				if (y == 0) {
+					y = 1;
+				}
+				if (y == h - 1) {
+					y = h - 2;
+				}
+				ex[x][y] = 0.25f * (e[0][x][(y + 1)] - e[0][x][y] + e[0][(x + 1)][(y + 1)] - e[0][(x + 1)][y]
+						+ e[1][x][(y + 1)] - e[1][x][y] + e[1][(x + 1)][(y + 1)] - e[1][(x + 1)][y]);
+				ey[x][y] = 0.25f * (e[0][(x + 1)][y] - e[0][x][y] + e[0][(x + 1)][(y + 1)] - e[0][x][(y + 1)]
+						+ e[1][(x + 1)][y] - e[1][x][y] + e[1][(x + 1)][(y + 1)] - e[1][x][(y + 1)]);
+				et[x][y] = 0.25f * (e[1][x][y] - e[0][x][y] + e[1][(x + 1)][y] - e[0][(x + 1)][y] + e[1][x][(y + 1)]
+						- e[0][x][(y + 1)] + e[1][(x + 1)][(y + 1)] - e[0][(x + 1)][(y + 1)]);
+			}
+		}
 
-                    if(x == 0)
-                        x = 1;
-                    if(x == w-1)
-                        x = w-2;
-                    if(y == 0)
-                        y = 1;
-                    if(y == h-1)
-                        y = h-2;
-                    ubar[x][y] = 1f / 6 * (u[x - 1][y] + u[x][y + 1] + u[x + 1][y] + u[x][y - 1])
-                          + 1f / 12 * (u[x - 1][y - 1] + u[x - 1][y + 1] + u[x + 1][y + 1] + u[x + 1][y - 1]);
+		for (int iter = 0; iter < NB_ITERATIONS; iter++) {
+			float[][] un = new float[w][h];
+			float[][] vn = new float[w][h];
 
-                    vbar[x][y] = 1f / 6 * (v[x - 1][y] + v[x][y + 1] + v[x + 1][y] + v[x][y - 1])
-                         + 1f / 12 * (v[x - 1][y - 1] + v[x - 1][y + 1] + v[x + 1][y + 1] + v[x + 1][y - 1]);
+			// compute mean velocity values
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					int x = i;
+					int y = j;
+					if (x == 0) {
+						x = 1;
+					}
+					if (x == w - 1) {
+						x = w - 2;
+					}
+					if (y == 0) {
+						y = 1;
+					}
+					if (y == h - 1) {
+						y = h - 2;
+					}
+					ubar[x][y] = 1f / 6 * (u[x - 1][y] + u[x][y + 1] + u[x + 1][y] + u[x][y - 1]) + 1f / 12
+							* (u[x - 1][y - 1] + u[x - 1][y + 1] + u[x + 1][y + 1] + u[x + 1][y - 1]);
+					vbar[x][y] = 1f / 6 * (v[x - 1][y] + v[x][y + 1] + v[x + 1][y] + v[x][y - 1]) + 1f / 12
+							* (v[x - 1][y - 1] + v[x - 1][y + 1] + v[x + 1][y + 1] + v[x + 1][y - 1]);
+				}
+			}
 
-                }
-            }
+			for (int x = 0; x < w; x++) {
+				for (int y = 0; y < h; y++) {
+					float exx = ex[x][y];
+					float ubarr = ubar[x][y];
+					float eyy = ey[x][y];
+					float vbarr = vbar[x][y];
+					float ett = et[x][y];
+					un[x][y] = ubarr - exx * (exx * ubarr + eyy * vbarr + ett)
+							/ (alpha * alpha + exx * exx + eyy * eyy);
+					vn[x][y] = vbarr - eyy * (exx * ubarr + eyy * vbarr + ett)
+							/ (alpha * alpha + exx * exx + eyy * eyy);
+				}
+			}
 
-            float[][] un = new float[w][h];
-            float[][] vn = new float[w][h];
+			u = un;
+			v = vn;
+		}
 
-            for( int x = 0; x < w; x++ ) {
-                for( int y = 0; y < h; y++ ) {
-                    float exx = ex[x][y];
-                    float ubarr = ubar[x][y];
-                    float eyy = ey[x][y];
-                    float vbarr = vbar[x][y];
-                    float ett = et[x][y];
-                    un[x][y] = ubarr - exx * (exx * ubarr + eyy * vbarr + ett) / (alpha * alpha + exx * exx + eyy * eyy);
-                    vn[x][y] = vbarr - eyy * (exx * ubarr + eyy * vbarr + ett) / (alpha * alpha + exx * exx + eyy * eyy);
-                }
-            }
+		// compute norms
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				int x = i;
+				int y = j;
+				if (x == 0) {
+					x = 1;
+				}
+				if (x == w - 1) {
+					x = w - 2;
+				}
+				if (y == 0) {
+					y = 1;
+				}
+				if (y == h - 1) {
+					y = h - 2;
+				}
+				float uxy = u[x][y];
+				float vxy = v[x][y];
+				norm[x][y] = (float) Math.sqrt(uxy * uxy + vxy * vxy);
+			}
+		}
+	}
 
-            u = un;
-            v = vn;
-        }
-    }
+	/** Expose the computed X velocities. */
+	public float[][] getU() {
+		return u;
+	}
 
-    public float[][] getU() {
-        return u;
-    }
+	/** Expose the computed Y velocities. */
+	public float[][] getV() {
+		return v;
+	}
 
-    public float[][] getV() {
-        return v;
-    }
+	/** Expose the computed velocity norms. */
+	public float[][] getNorm() {
+		return norm;
+	}
 }
--- vvUI.java
+++ vvUI.java
@@ -18,251 +18,304 @@ import javax.swing.JTextArea;
 
 import payback.Environment;
 
+@SuppressWarnings("serial")
 public class vvUI extends AppletUI {
-    private JPanel spritePanel;
-    private JPanel flowPanel;
-    private InputHandler joy1 = new vvInputHandler();
-    private Environment environment;
+	private JPanel spritePanel;
+	private JPanel flowPanel;
+	private InputHandler joy1 = new vvInputHandler();
+	private Environment environment;
 
-    public vvUI( vNES applet ) {
-        super( applet );
+	public vvUI(vNES applet) {
+		super(applet);
 
-        try {
-            environment = new Environment();
-        } catch( Exception e ) {
-            e.printStackTrace();
-            System.exit( 666 );
-        }
-        spritePanel = new DancingSugarPlumFairiesPanel();
-        flowPanel = new FlowPanel();
-        showFrame( 300, 0, spritePanel );
-        showFrame( 300, 300, flowPanel );
-        showFrame( 800, 0, new MagicAddressDivinationWizardPanel() );
+		try {
+			environment = new Environment();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(666);
+		}
+		spritePanel = new DancingSugarPlumFairiesPanel();
+		flowPanel = new FlowPanel();
+		showFrame(300, 0, spritePanel);
+		showFrame(300, 300, flowPanel);
+		showFrame(800, 0, new MagicAddressDivinationWizardPanel());
 
-    }
+	}
 
-    private void showFrame( int x, int y, JComponent component ) {
-        JFrame jFrame = new JFrame();
-        jFrame.add( component );
-        jFrame.pack();
-        jFrame.setLocation( x, y );
-        jFrame.setVisible( true );
-    }
+	private void showFrame(int x, int y, JComponent component) {
+		JFrame jFrame = new JFrame();
+		jFrame.add(component);
+		jFrame.pack();
+		jFrame.setLocation(x, y);
+		jFrame.setVisible(true);
+	}
 
-    @Override
-    public void imageReady( boolean skipFrame ) {
-        super.imageReady( skipFrame );
-        spritePanel.repaint();
-        flowPanel.repaint();
-    }
+	@Override
+	public void imageReady(boolean skipFrame) {
+		super.imageReady(skipFrame);
+		spritePanel.repaint();
+		flowPanel.repaint();
+	}
 
-    // @Override
-    // public InputHandler getJoy1() {
-    // return joy1;
-    // }
+	// @Override
+	// public InputHandler getJoy1() {
+	// return joy1;
+	// }
 
-    private class DancingSugarPlumFairiesPanel extends JPanel {
-        private static final double SCALE_FACTOR = 1.5;
+	private class DancingSugarPlumFairiesPanel extends JPanel {
+		private static final double SCALE_FACTOR = 1.5;
 
-        public DancingSugarPlumFairiesPanel() {
-            setPreferredSize( new Dimension( (int) (256 * SCALE_FACTOR), (int) (240 * SCALE_FACTOR) ) );
-        }
+		public DancingSugarPlumFairiesPanel() {
+			setPreferredSize(new Dimension((int) (256 * SCALE_FACTOR), (int) (240 * SCALE_FACTOR)));
+		}
 
-        @Override
-        protected void paintComponent( Graphics g ) {
-            super.paintComponent( g );
-            g.setFont( new Font( "sans serif", Font.PLAIN, 10 ) );
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setFont(new Font("sans serif", Font.PLAIN, 10));
 
-            PPU ppu = nes.getPpu();
+			PPU ppu = nes.getPpu();
 
-            // int currentNametable = ppu.ntable1[0];
-            NameTable nameTable = ppu.nameTable[0];
-            int baseTile = (ppu.regS == 0 ? 0 : 256);
-            for( int i = 0; i < 32; i++ ) {
-                for( int j = 0; j < 30; j++ ) {
-                    short tile = nameTable.getTileIndex( i, j );
-                    int x = i * 8;
-                    int y = j * 8;
-                    nes.ppu.ptTile[baseTile + tile].renderSimple( x, y, g, false, false );
-                }
-            }
+			// int currentNametable = ppu.ntable1[0];
+			NameTable nameTable = ppu.nameTable[0];
+			int baseTile = (ppu.regS == 0 ? 0 : 256);
+			for (int i = 0; i < 32; i++) {
+				for (int j = 0; j < 30; j++) {
+					short tile = nameTable.getTileIndex(i, j);
+					int x = i * 8;
+					int y = j * 8;
+					nes.ppu.ptTile[baseTile + tile].renderSimple(x, y, g, false, false);
+				}
+			}
 
-            for( int i = 0; i < 64; i++ ) {
-                int x = ppu.sprX[i];
-                int y = ppu.sprY[i];
-                int tile = ppu.sprTile[i];
-                boolean mirrorH = nes.ppu.horiFlip[i];
-                boolean mirrorV = nes.ppu.vertFlip[i];
+			for (int i = 0; i < 64; i++) {
+				int x = ppu.sprX[i];
+				int y = ppu.sprY[i];
+				int tile = ppu.sprTile[i];
+				boolean mirrorH = nes.ppu.horiFlip[i];
+				boolean mirrorV = nes.ppu.vertFlip[i];
 
-                if( x >= 0 && x < 256 && y >= 0 && y < 240 ) {
-                    nes.ppu.ptTile[tile].renderSimple( x, y, g, mirrorV, mirrorH );
-                }
-            }
+				if (x >= 0 && x < 256 && y >= 0 && y < 240) {
+					nes.ppu.ptTile[tile].renderSimple(x, y, g, mirrorV, mirrorH);
+				}
+			}
 
-            if( mmRect != null ) {
-                g.setColor( Color.RED );
-                g.drawRect( mmRect.x, mmRect.y, mmRect.width, mmRect.height );
-            }
+			if (mmRect != null) {
+				g.setColor(Color.RED);
+				g.drawRect(mmRect.x, mmRect.y, mmRect.width, mmRect.height);
+			}
 
-        }
-    }
+		}
+	}
 
-    private class FlowPanel extends JPanel {
-        private static final double SCALE_FACTOR = 5;
+	private class FlowPanel extends JPanel {
+		private static final double SCALE_FACTOR = 3;
 
-        public FlowPanel() {
-            setPreferredSize( new Dimension( (int) (256 * SCALE_FACTOR), (int) (240 * SCALE_FACTOR) ) );
-        }
+		public FlowPanel() {
+			setPreferredSize(new Dimension((int) (256 * SCALE_FACTOR), (int) (240 * SCALE_FACTOR)));
+		}
 
-        @Override
-        protected void paintComponent( Graphics g ) {
-            super.paintComponent( g );
+		/**
+		 * Computes the scalar product of velocities at (i,j) and (k,l).
+		 */
+		private float scalar(float[][] u, float[][] v, int i, int j, int k, int l) {
+			if (k < 0) {
+				k = 0;
+			}
+			if (l < 0) {
+				l = 0;
+			}
+			if (k > u.length - 1) {
+				k = u.length - 1;
+			}
+			if (l > u[0].length - 1) {
+				l = u[0].length - 1;
+			}
+			return u[i][j] * u[k][l] + v[i][j] * v[k][l];
+		}
 
-            float[][] u = environment.getFlow().getU();
-            float[][] v = environment.getFlow().getV();
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
 
-            if( u == null || v == null )
-                return;
+			float[][] u = environment.getFlow().getU();
+			float[][] v = environment.getFlow().getV();
+			float[][] n = environment.getFlow().getNorm();
 
-            g.setColor( Color.black );
+			if (u == null || v == null || n == null) {
+				return; // avoid NPE
+			}
 
-            float maxNorm = 0;
-            for( int i = 0; i < u.length; i++ ) {
-                for( int j = 0; j < u[0].length; j++ ) {
-                    float uij = u[i][j];
-                    float vij = v[i][j];
-                    maxNorm = Math.max( maxNorm,uij*uij+vij*vij );
-                }
-            }
-            maxNorm = (float) Math.sqrt( maxNorm );
+			float maxNorm = 0;
+			for (int i = 0; i < u.length; i++) {
+				for (int j = 0; j < u[0].length; j++) {
+					maxNorm = Math.max(maxNorm, n[i][j]);
+				}
+			}
 
-            for( int i = 0; i < u.length; i++ ) {
-                for( int j = 0; j < u[0].length; j++ ) {
-//                    g.setColor( new Color( Color.HSBtoRGB( 0f, 1f, (u[i][j] * u[i][j] + v[i][j] * v[i][j]) / 10 ) ) );
-//                    g.drawRect( i, j, 1, 1 );
+			// just for fun (may help define a threshold ?)
+			// showVelocityHistogram(g, u, v, maxNorm);
 
-                    float uij = u[i][j];
-                    float vij = v[i][j];
-                    float norm = (float) Math.sqrt( uij*uij+vij*vij );
-                    double x = i*SCALE_FACTOR;
-                    double y = j*SCALE_FACTOR;
-                    g.setColor(new Color( Color.HSBtoRGB( 0, 0, norm %256 ) ));
-                    if( norm > 0.5*maxNorm) {
-                        g.setColor( Color.red );
-                    } else if (norm < .25*maxNorm) {
-                        g.setColor( Color.blue );
-                    } else {
-                        g.setColor( Color.black );
-                    }
-//                    if(norm > 0.00001f)
-                    g.drawLine( (int)x, (int)y, (int)(x+uij*3/maxNorm), (int) (y+vij*3/maxNorm) );
+			for (int i = 0; i < u.length; i++) {
+				for (int j = 0; j < u[0].length; j++) {
+					float uij = u[i][j];
+					float vij = v[i][j];
+					float norm = n[i][j];
+					double x = i * SCALE_FACTOR;
+					double y = j * SCALE_FACTOR;
 
-                }
-            }
+					g.setColor(new Color(Color.HSBtoRGB(0, 0, norm * 256 / maxNorm)));
+					if (norm > 0.5 * maxNorm) {
+						g.setColor(Color.red);
+					} else if (norm < .25 * maxNorm) {
+						g.setColor(Color.blue);
+					} else {
+						g.setColor(Color.black);
+					}
 
+					// if(norm > 0.00001f)
+					g.drawLine((int) x, (int) y, (int) (x + uij * SCALE_FACTOR * .7 / maxNorm), (int) (y + vij
+							* SCALE_FACTOR * .7 / maxNorm));
 
-        }
-    }
+					// find area of same velocity
+					g.setColor(Color.CYAN);
+					double threshold = .3; // related to the histogram ?
+					for (int p = -1; p < 2; p++) {
+						for (int q = -1; q < 2; q++) {
+							if ((p != 0 || q != 0) && Math.sqrt(scalar(u, v, i, j, i + p, j + q)) > threshold * maxNorm) {
+								g.drawRect((int) x - 2, (int) y - 2, 5, 5);
+							}
+						}
+					}
 
-    private class MagicAddressDivinationWizardPanel extends JPanel {
-        private static final int RAM_SIZE = 0x800;
-        private short[] snapshot = new short[RAM_SIZE];
-        private boolean[] matches = new boolean[RAM_SIZE];
-        private JTextArea textArea;
+				}
+			}
 
-        public MagicAddressDivinationWizardPanel() {
-            setLayout( new BoxLayout( this, BoxLayout.PAGE_AXIS ) );
-            add( new JButton( new StartAnalysisAction() ) );
-            add( new JButton( new FindChangesAction() ) );
-            add( new JButton( new ExcludeChangesAction() ) );
-            textArea = new JTextArea( 20, 50 );
-            add( new JScrollPane( textArea ) );
-        }
+		}
 
-        private short[] getCPUMemory() {
-            short[] mem = new short[0x800];
-            System.arraycopy( nes.cpuMem.mem, 0, mem, 0, RAM_SIZE );
-            return mem;
-        }
+		/**
+		 * Shows a velocity histogram based on a 0 (slowest) - 250 (fastest)
+		 * scale.
+		 */
+		private void showVelocityHistogram(Graphics g, float[][] u, float[][] v, float maxNorm) {
+			int[] histo = new int[256];
+			float histoStep = maxNorm / 250;
+			for (int i = 0; i < u.length; i++) {
+				for (int j = 0; j < u[0].length; j++) {
+					float uij = u[i][j];
+					float vij = v[i][j];
+					int k = (int) (Math.sqrt(uij * uij + vij * vij) / histoStep);
+					histo[k]++;
+				}
+			}
+			g.setColor(Color.GREEN);
+			for (int i = 0; i < histo.length; i++) {
+				g.drawLine(i, 0, i, histo[i]);
+			}
+		}
+	}
 
-        private void displayMatches() {
-            int matchCount = 0;
-            for( int i = 0; i < matches.length; i++ ) {
-                if( matches[i] )
-                    matchCount++;
-            }
-            textArea.append( "Found " + matchCount + " potential matches.\n" );
-            if( matchCount <= 25 ) {
-                for( int i = 0; i < RAM_SIZE; i++ ) {
-                    if( matches[i] ) {
-                        textArea.append( "Address: 0x" + Integer.toHexString( i ) + ", value: 0x" + Integer.toHexString( snapshot[i] ) + "\n" );
-                    }
-                }
-            }
-            textArea.scrollRectToVisible( new Rectangle( 0, textArea.getHeight() - 2, 1, 1 ) );
+	private class MagicAddressDivinationWizardPanel extends JPanel {
+		private static final int RAM_SIZE = 0x800;
+		private short[] snapshot = new short[RAM_SIZE];
+		private boolean[] matches = new boolean[RAM_SIZE];
+		private JTextArea textArea;
 
-        }
+		public MagicAddressDivinationWizardPanel() {
+			setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+			add(new JButton(new StartAnalysisAction()));
+			add(new JButton(new FindChangesAction()));
+			add(new JButton(new ExcludeChangesAction()));
+			textArea = new JTextArea(20, 50);
+			add(new JScrollPane(textArea));
+		}
 
-        private class StartAnalysisAction extends AbstractAction {
+		private short[] getCPUMemory() {
+			short[] mem = new short[0x800];
+			System.arraycopy(nes.cpuMem.mem, 0, mem, 0, RAM_SIZE);
+			return mem;
+		}
 
-            public StartAnalysisAction() {
-                super( "(Re)Start analysis" );
-            }
+		private void displayMatches() {
+			int matchCount = 0;
+			for (int i = 0; i < matches.length; i++) {
+				if (matches[i])
+					matchCount++;
+			}
+			textArea.append("Found " + matchCount + " potential matches.\n");
+			if (matchCount <= 25) {
+				for (int i = 0; i < RAM_SIZE; i++) {
+					if (matches[i]) {
+						textArea.append("Address: 0x" + Integer.toHexString(i) + ", value: 0x"
+								+ Integer.toHexString(snapshot[i]) + "\n");
+					}
+				}
+			}
+			textArea.scrollRectToVisible(new Rectangle(0, textArea.getHeight() - 2, 1, 1));
 
-            public void actionPerformed( ActionEvent e ) {
-                snapshot = getCPUMemory();
-                Arrays.fill( matches, true );
-                textArea.append( "Analysis started.\n" );
-            }
-        }
+		}
 
-        private class FindChangesAction extends AbstractAction {
+		private class StartAnalysisAction extends AbstractAction {
 
-            public FindChangesAction() {
-                super( "Find addresses that have changed" );
-            }
+			public StartAnalysisAction() {
+				super("(Re)Start analysis");
+			}
 
-            public void actionPerformed( ActionEvent e ) {
-                short[] mem = getCPUMemory();
-                for( int i = 0; i < RAM_SIZE; i++ ) {
-                    if( snapshot[i] == mem[i] )
-                        matches[i] = false;
-                }
-                snapshot = mem;
-                displayMatches();
-            }
+			public void actionPerformed(ActionEvent e) {
+				snapshot = getCPUMemory();
+				Arrays.fill(matches, true);
+				textArea.append("Analysis started.\n");
+			}
+		}
 
-        }
+		private class FindChangesAction extends AbstractAction {
 
-        private class ExcludeChangesAction extends AbstractAction {
+			public FindChangesAction() {
+				super("Find addresses that have changed");
+			}
 
-            public ExcludeChangesAction() {
-                super( "Find addresses that do not have changed" );
-            }
+			public void actionPerformed(ActionEvent e) {
+				short[] mem = getCPUMemory();
+				for (int i = 0; i < RAM_SIZE; i++) {
+					if (snapshot[i] == mem[i])
+						matches[i] = false;
+				}
+				snapshot = mem;
+				displayMatches();
+			}
 
-            public void actionPerformed( ActionEvent e ) {
-                short[] mem = getCPUMemory();
-                for( int i = 0; i < RAM_SIZE; i++ ) {
-                    if( snapshot[i] != mem[i] )
-                        matches[i] = false;
-                }
-                snapshot = mem;
-                displayMatches();
-            }
-        }
+		}
 
-    }
+		private class ExcludeChangesAction extends AbstractAction {
 
-    private Rectangle mmRect;
-    private long frameLimiter = 0;
+			public ExcludeChangesAction() {
+				super("Find addresses that do not have changed");
+			}
 
-    @Override
-    public void onEndFrame() {
-//        if( frameLimiter++ % 10 == 0 ) {
-            long start = System.currentTimeMillis();
-            environment.update( (BufferedImage) getScreenView().getImage() );
-            System.out.println(System.currentTimeMillis()-start);
-//        }
-    }
+			public void actionPerformed(ActionEvent e) {
+				short[] mem = getCPUMemory();
+				for (int i = 0; i < RAM_SIZE; i++) {
+					if (snapshot[i] != mem[i])
+						matches[i] = false;
+				}
+				snapshot = mem;
+				displayMatches();
+			}
+		}
+
+	}
+
+	private Rectangle mmRect;
+
+	// private long frameLimiter = 0;
+
+	@Override
+	public void onEndFrame() {
+		// if( frameLimiter++ % 10 == 0 ) {
+		long start = System.currentTimeMillis();
+		environment.update((BufferedImage) getScreenView().getImage());
+		System.out.println(System.currentTimeMillis() - start);
+		// }
+	}
 
 }
